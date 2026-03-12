a4e687de7655d8fe820cb5d6d9fbf28f928e7cac
Arthur Henrique <110206900+ArthurHSS2@users.noreply.github.com>
2026-03-12 16:46:46 -0300

    Add files via upload

---
+++ tb_mux.sv
@@ -0,0 +1,52 @@
+`timescale 1ns/1ps
+
+module tb_mux;
+
+logic [31:0] a, b, c, d;
+logic [31:0] muxOut;
+logic sel1, sel2;
+
+genvar i;
+
+generate
+  for(i=0;i<32;i++) begin : mux_array
+    mux dut(
+      .f(muxOut[i]),
+      .a(a[i]),
+      .b(b[i]),
+      .c(c[i]),
+      .d(d[i]),
+      .sel1(sel1),
+      .sel2(sel2)
+    );
+  end
+endgenerate
+
+initial begin
+
+  // Valores bonitos para as entradas
+  a = 32'hDEAD_BEEF;  // Valor clássico de "dead beef"
+  b = 32'hCAFEBABE;  // "Cafe Babe" (muito conhecido no mundo da programação)
+  c = 32'hBADD_C0DE;  // Um código ruim, mas bonito
+  d = 32'hFACE_CAFE;  // Outra combinação popular, "Face Cafe"
+  
+  $monitor($time," sel2=%b sel1=%b muxOut=%h",sel2,sel1,muxOut);
+
+  // Testando todas as combinações de sel
+  sel2 = 0; sel1 = 0;  // Caso 1: selecione `a`
+  #10;
+
+  sel2 = 0; sel1 = 1;  // Caso 2: selecione `b`
+  #10;
+
+  sel2 = 1; sel1 = 0;  // Caso 3: selecione `c`
+  #10;
+
+  sel2 = 1; sel1 = 1;  // Caso 4: selecione `d`
+  #10;
+
+  $stop;
+
+end
+
+endmodule
